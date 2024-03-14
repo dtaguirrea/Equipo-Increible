eff45de37f98d27e9bf8625ba3d2d60ddf151f46
GonDzalo <163420463+GonDzalo@users.noreply.github.com>
2024-03-14 15:36:43 +0000

    solucione parimpar

--- juegos/adivinar_par_o_impar.py
+++ juegos/adivinar_par_o_impar.py
@@ -1,8 +1,29 @@
-def adivinar_par_o_impar():
-    """
-    Esta función representa el juego de adivinar si un número es par o impar.
-    Tienes que permitir que el usuario ingrese una de las dos opciones y
-    generar un número aleatorio para ver si es par o impar.
-    Se debe mostrar si el usuario adivina correctamente o no.
-    """
-    pass
+def adivinar_par_o_impar(numero):
+    import random
+    par_impar=""
+    numero_random= random.randint(-999,999)
+    if numero_random/2==numero_random//2:
+        par_impar="par"
+    else:
+        par_impar="impar"
+    if numero==par_impar:
+        numero=True
+    print("A salido", numero_random, "que es", par_impar)
+    if numero==True:
+        return ("Adivinaste")
+    else:
+        return ("Manqueque")
+print("Si quieres jugar par o impar, di ""Es hora de ser heroe""")
+respuesta=input()
+if respuesta!="Es hora de ser heroe":
+    print("bueno pues :(")
+if respuesta=="Es hora de ser heroe":
+    print("¿Será par o impar?")
+    adivina=input()
+    while adivina !="par" and adivina != "impar":
+        print("Chistoso, pero no tanto")
+        print("¿Será par o impar?")
+        adivina=input()
+    print("La ruleta esta girando")
+    print(adivinar_par_o_impar(adivina))
+    
