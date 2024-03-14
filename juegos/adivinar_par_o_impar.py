b953bd9998fbcb7b7485d42aceb34aa91c430f04
Javierpuffles <163352921+Javierpuffles@users.noreply.github.com>
2024-03-14 15:51:17 +0000

    Arreglado par impar

--- juegos/adivinar_par_o_impar.py
+++ juegos/adivinar_par_o_impar.py
@@ -1,29 +1,31 @@
-def adivinar_par_o_impar(numero):
+def adivinar_par_o_impar():
+    print("Si quieres jugar par o impar, di ""Es hora de ser heroe""")
+    respuesta=input()
+    if respuesta!="Es hora de ser heroe":
+        print("bueno pues :(")
+        return()
+    if respuesta=="Es hora de ser heroe":
+        print("¿Será par o impar?")
+        adivina=input()
+        while adivina !="par" and adivina != "impar":
+            print("Chistoso, pero no tanto")
+            print("¿Será par o impar?")
+            adivina=input()
+        print("La ruleta esta girando")
     import random
     par_impar=""
+    numero = False
     numero_random= random.randint(-999,999)
     if numero_random/2==numero_random//2:
         par_impar="par"
     else:
         par_impar="impar"
-    if numero==par_impar:
-        numero=True
+    if numero_random==par_impar:
+        numero = True
     print("A salido", numero_random, "que es", par_impar)
-    if numero==True:
-        return ("Adivinaste")
+    if par_impar==adivina:
+        print("Adivinaste")
     else:
-        return ("Manqueque")
-print("Si quieres jugar par o impar, di ""Es hora de ser heroe""")
-respuesta=input()
-if respuesta!="Es hora de ser heroe":
-    print("bueno pues :(")
-if respuesta=="Es hora de ser heroe":
-    print("¿Será par o impar?")
-    adivina=input()
-    while adivina !="par" and adivina != "impar":
-        print("Chistoso, pero no tanto")
-        print("¿Será par o impar?")
-        adivina=input()
-    print("La ruleta esta girando")
-    print(adivinar_par_o_impar(adivina))
+        print("Manqueque")
+
     
