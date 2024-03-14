95e8ce9ab07e4e7374883ca07d6a76d2e99a745e
Daniel Aguirre <main.data06@gmail.com>
2024-03-14 13:01:00 -0300

    cambios adivinar numero

--- juegos/adivinar_numero.py
+++ juegos/adivinar_numero.py
@@ -1,12 +1,9 @@
 import random
 def adivinar_numero():
     cpu =  random.randint(1,10)
-    print(cpu)
     usuario= int(input("Adivina el número en el que estoy pensando "))
     if usuario == cpu:
         print("Lo adivinaste!")
     else:
         print("Trolleaste")
     pass
-
-adivinar_numero()
