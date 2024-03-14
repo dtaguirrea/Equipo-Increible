a2ab794faa785eab687b152bbca240f71fdda610
Yakao01 <163423806+Yakao01@users.noreply.github.com>
2024-03-14 16:18:58 +0000

    Arreglo adivinar

--- juegos/adivinar_numero.py
+++ juegos/adivinar_numero.py
@@ -2,10 +2,11 @@ import random
 import time
 def adivinar_numero():
     cpu =  random.randint(1,10)
-    usuario= int(input("Adivina el número en el que estoy pensando "))
+    usuario= int(input("Adivina el número en el que estoy pensando  entre el 1 al 10"))
     if usuario == cpu:
         print("Lo adivinaste!")
     else:
         print("Trolleaste")
+    print( 'El numero era ' + str(cpu))
     time.sleep(2)
     pass
